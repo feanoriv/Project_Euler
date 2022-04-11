9b98f8b864e0c9b29dddc0d84cb40b2c5294748f
PC <feanoriv@mail.ru>
2022-04-11 09:28:39 +0300

    Задача 17

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -429,4 +429,53 @@ def sum_degree_of_2(n:int) -> int:  # Сумма цифр в числе 2^n
     for num in str_number:
         res += int(num)
     return res
-print(sum_degree_of_2(1000))
+# print(sum_degree_of_2(1000))
+
+
+"""
+Задача 17
+Если записать числа от 1 до 5 английскими словами (one, two, three, four, five), 
+то используется всего 3 + 3 + 5 + 4 + 4 = 19 букв.
+Сколько букв понадобится для записи всех чисел от 1 до 1000 (one thousand) включительно?
+Примечание: Не считайте пробелы и дефисы. Например, число 342 (three hundred and forty-two) 
+состоит из 23 букв, число 115 (one hundred and fifteen) - из 20 букв. 
+Использование "and" при записи чисел соответствует правилам британского английского."""
+# dict из просторов
+ones = {
+    0: '', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six',
+    7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve',
+    13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen',
+    17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+tens = {
+    2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty', 6: 'sixty',
+    7: 'seventy', 8: 'eighty', 9: 'ninety'}
+illions = {
+    1: 'thousand', 2: 'million', 3: 'billion', 4: 'trillion', 5: 'quadrillion',
+    6: 'quintillion', 7: 'sextillion', 8: 'septillion', 9: 'octillion',
+    10: 'nonillion', 11: 'decillion'}
+@timer
+def how_letter_in_sequence(n):
+    res = 0
+    #res_str = []
+    for i in range(1, n+1):
+        if i // 10 <= 1:
+            res += len(ones[i])
+            #res_str.append(ones[i])
+        elif i // 10 >= 2 and i // 10 <= 9:
+            res += len(tens[i // 10]) + len(ones[i % 10])
+            #res_str.append(tens[i // 10] + " " + ones[i % 10])
+        elif i // 10 > 9 and i // 10 < 100:
+            if i % 100 == 0:
+                res += len(ones[i / 100]) + len("hundred")
+                #res_str.append(ones[i / 100] + " " + "hundred")
+            elif i % 100 <= 19:
+                res += len(ones[i // 100]) + len("hundred") + len("and") + len(ones[i % 100])
+                #res_str.append(ones[i // 100] + " " + "hundred" + " and" + " " + ones[i % 100])
+            elif i % 100 >= 19:
+                res += len(ones[i // 100]) + len("hundred") + len("and") + len(tens[(i % 100) // 10]) + len(ones[(i % 100) % 10])
+                #res_str.append(ones[i // 100] + " " + "hundred" + " and " + tens[(i % 100) // 10] + " " + ones[(i % 100) % 10])
+        elif i // 1000 >= 1:
+            res += len("onethousand")
+    return res
+# Закоменчен список текстового представления элементов
+print(how_letter_in_sequence(1000))
