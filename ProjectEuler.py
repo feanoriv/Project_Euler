4c9951d27bbdecb65ef3f83c538b25d08dcc335a
PC <feanoriv@mail.ru>
2022-04-12 13:43:23 +0300

    Задача 20

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -569,3 +569,21 @@ def sundays_count():
 # Минут 30 не понимал откуда лишний день взялся, нашёл пару схожих проблем на stuckoverflow, и что в итоге?
 # Я считал понедельники, а не воскресенья...
 # print(sundays_count())
+
+
+"""
+Задача 20
+n! означает n × (n − 1) × ... × 3 × 2 × 1
+Например, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+и сумма цифр в числе 10! равна 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Найдите сумму цифр в числе 100!.
+"""
+@timer
+def factorial_sum(n):
+    factorial = 1
+    for i in range(1, n+1):
+        factorial *= i
+    return sum([int(x) for x in list(str(factorial))])
+
+# print(factorial_sum(100))
+
