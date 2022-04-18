413b6c669f3b6f92585df89031f9f980804814b6
PC <feanoriv@mail.ru>
2022-04-18 15:55:25 +0300

    Задача 34

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -1197,4 +1197,26 @@ def problem_33():
     return num2
 
 
-print(problem_33())
+# print(problem_33())
+
+
+
+"""
+Задача 34
+145 является любопытным числом, поскольку 1! + 4! + 5! = 1 + 24 + 120 = 145.
+Найдите сумму всех чисел, каждое из которых равно сумме факториалов своих цифр.
+Примечание: поскольку 1! = 1 и 2! = 2 не являются суммами, учитывать их не следует.
+"""
+@timer
+def problem_34():
+    res = 0
+    # TODO почему такой лимит? Зная ответ можно вписать 41000 и решение будет за 0,5 с., а с этим лимитом 16 с.
+    limit = 7 * factorial(9) + 1
+    for i in range(3, limit):
+        sum_factorials = sum([factorial(int(i)) for i in list(str(i))])
+        if sum_factorials == i:
+            print(i)
+            res += i
+    return res
+
+# print(problem_34())
