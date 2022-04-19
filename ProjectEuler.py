3c59b75ef33447e7de4d8c4a1d6ec135b11de523
PC <feanoriv@mail.ru>
2022-04-19 10:04:51 +0300

    Задача 37

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -1282,3 +1282,43 @@ def problem_36(n=1000000):
 
 
 # print(problem_36())
+
+
+"""
+Задача 37
+Число 3797 обладает интересным свойством. Будучи само по себе простым числом, 
+из него можно последовательно выбрасывать цифры слева направо, число же при этом 
+остается простым на каждом этапе: 3797, 797, 97, 7. Точно таким же способом можно 
+выбрасывать цифры справа налево: 3797, 379, 37, 3.
+Найдите сумму единственных одиннадцати простых чисел, из которых можно выбрасывать 
+цифры как справа налево, так и слева направо, но числа при этом остаются простыми.
+ПРИМЕЧАНИЕ: числа 2, 3, 5 и 7 таковыми не считаются.
+"""
+
+def check_variations_is_prime_2(n: str):
+    if len(n) == 1:
+        return False
+    if n.endswith("1") or n.startswith("1"):
+        return False
+    if any(num in ["0", "4", "6", "8"] for num in n):
+        return False
+    for i in range(len(n)-1):
+        n1 = n[i + 1:]
+        n2 = n[:-i-1]
+        if not is_prime(int(n1)):
+            return False
+        if not is_prime(int(n2)):
+            return False
+    return True
+
+
+@timer
+def problem_37(n=1000000):
+    res = 0
+    for i in range(11, n, 2):
+        if is_prime(i):
+            if check_variations_is_prime_2(str(i)):
+                res += i
+    return res
+
+# print(problem_37())
