2cce53182c35bb1a379bf274cf891d6028a90307
PC <feanoriv@mail.ru>
2022-04-19 09:17:33 +0300

    Задача 35

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -1220,3 +1220,36 @@ def problem_34():
     return res
 
 # print(problem_34())
+
+
+"""
+Задача 25
+Число 197 называется круговым простым числом, потому что все перестановки его цифр 
+с конца в начало являются простыми числами: 197, 719 и 971.
+Существует тринадцать таких простых чисел меньше 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79 и 97.
+Сколько существует круговых простых чисел меньше миллиона?
+"""
+
+def check_variations_is_prime(n: str):
+    if len(n) == 1:
+        return True
+    if any(num in ["0", "2", "4", "6", "8"] for num in n):
+        return False
+    for i in range(len(n)):
+        n = n[1:] + n[0]
+        if not is_prime(int(n)):
+            return False
+    return True
+
+
+@timer
+def problem_35(n=1000000):
+    res = 0
+    for i in range(2, n):
+        if is_prime(i):
+            if check_variations_is_prime(str(i)):
+                res += 1
+    return res
+# print(problem_35())
+
+
